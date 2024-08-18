2027357f472165e975a0bec4d2af68bbbd257c5b
oooozzzzz <lobovdima27@gmail.com>
2024-08-18 15:00:14 +0300

    //

--- app/api/cart/route.ts
+++ app/api/cart/route.ts
@@ -1,24 +1,26 @@
-import { prisma } from '@/libs/prisma';
-import { NextResponse } from 'next/server';
+import { prisma } from "@/libs/prisma";
+import { NextResponse } from "next/server";
 
 export async function GET() {
-  const cart = await prisma.cart.findFirst({
-    where: {
-      userId: 1,
-    },
-    include: {
-      items: {
-        include: {
-          pizza: {
-            include: {
-              product: true,
-            },
-          },
-          ingredients: true,
-        },
-      },
-    },
-  });
+	try {
+		const cart = await prisma.cart.findFirst({
+			where: {
+				userId: 1,
+			},
+			include: {
+				items: {
+					include: {
+						pizza: {
+							include: {
+								product: true,
+							},
+						},
+						ingredients: true,
+					},
+				},
+			},
+		});
 
-  return NextResponse.json(cart);
+		return NextResponse.json(cart);
+	} catch (error) {}
 }
